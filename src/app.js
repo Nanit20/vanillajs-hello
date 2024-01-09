e6c30a2c5811a14409e8cfc12b93a643ee022025
Nanit20 <55482552+Nanit20@users.noreply.github.com>
2024-01-09 06:51:40 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["El perro", "Mi abuela", "Su tortuga", "Mi pájaro"];
+let action = ["se comió", "orinó", "perdió", "rompió"];
+let what = ["mis deberes", "las llaves", "el coche"];
+let when = ["antes de llegar a clase", "justo a tiempo", "mientras comía"];
+function dameExcusa() {
+  let frase =
+    who[Math.floor(Math.random() * 4)] +
+    " " +
+    action[Math.floor(Math.random() * 4)] +
+    " " +
+    what[Math.floor(Math.random() * 3)] +
+    " " +
+    when[Math.floor(Math.random() * 3)];
+  return frase;
+}
 window.onload = function() {
   //write your code here
+  document.getElementById("excusa").innerHTML = dameExcusa();
+
   console.log("Hello Rigo from the console!");
 };
